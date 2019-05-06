da997be2efe409fb666d0ffcb5f337a2f4103b1b
Benoit Pasquier <pasquieb@uci.edu>
2019-05-06 21:25:00 +1000

    fix test and Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,14 +8,14 @@ WOAT = WorldOceanAtlasTools
 ENV["DATADEPS_ALWAYS_ACCEPT"] = true
 
 @testset "Downloading" begin
-    years = [2013, 2018] # No NetCDF at these URLs for 2009 data
-    vvs = ["p", "i", "n"]
+    years = [2013, 2018] # WOA years with NetCDF at these URLs
+    vvs = ["p", "i", "n"] # nutrients only
     tts = collect(0:16)
     ggs = ["5°", "1°"]
 
     @testset " WOA$(WOAT.my_year(year))" for year in years
         @testset "Downloading $gg x $gg" for gg in ggs
-            @testset " $(WOAT.my_averaging_period(tt))" for tt in tts[[1,2,13]]
+            @testset " $(WOAT.my_averaging_period(tt))" for tt in tts
                 @testset " $(WOAT.WOA_path_varname(vv))" for vv in vvs
                     woa_lat, woa_lon, woa_μ_2D = WOAT.WOA_surface_map(year, vv, tt, gg)
                     @test woa_μ_2D isa Array{<:AbstractFloat, 2}

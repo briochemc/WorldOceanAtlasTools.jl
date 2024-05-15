aa234daf02161bc3d982e1f7a4528484005e416d
gaelforget <gforget@mit.edu>
2024-05-15 11:33:24 -0400

    add support for WOA23

--- src/citations.jl
+++ src/citations.jl
@@ -20,6 +20,7 @@ citation_Temperature(product_year) = @match my_product_year(product_year) begin
     "09" => "Locarnini et al. (2010), World Ocean Atlas 2009, Volume 1: Temperature. S. Levitus, Ed. NOAA Atlas NESDIS 68, U.S. Government Printing Office, Washington, D.C., 184 pp."
     "13" => "Locarnini et al. (2013), World Ocean Atlas 2013, Volume 1: Temperature. S. Levitus, Ed., A. Mishonov Technical Ed.; NOAA Atlas NESDIS 73, 40 pp."
     "18" => "Locarnini et al. (2018), World Ocean Atlas 2018, Volume 1: Temperature. A. Mishonov Technical Ed.; in preparation."
+    "23" => "Locarnini et al. (2023), World Ocean Atlas 2023, Volume 1: Temperature. A. Mishonov Technical Ed.; NOAA Atlas NESDIS 89, 52 pp, https://doi.org/10.25923/54bh-1613"
     _ => error("No citation for WOA$(my_product_year(product_year)) Temperature. Add it if it should be there!")
 end
 
@@ -27,6 +28,7 @@ citation_Salinity(product_year) = @match my_product_year(product_year) begin
     "09" => "Antonov et al. (2010), World Ocean Atlas 2009, Volume 2: Salinity. S. Levitus, Ed. NOAA Atlas NESDIS 69, U.S. Government Printing Office, Washington, D.C., 184 pp."
     "13" => "Zweng et al. (2013), World Ocean Atlas 2013, Volume 2: Salinity. S. Levitus, Ed., A. Mishonov Technical Ed.; NOAA Atlas NESDIS 74, 39 pp."
     "18" => "Zweng et al. (2018), World Ocean Atlas 2018, Volume 2: Salinity. A. Mishonov Technical Ed.; in preparation."
+    "23" => "Reagan, et al. (2023), World Ocean Atlas 2023, Volume 2: Salinity. A. Mishonov Technical Ed.; NOAA Atlas NESDIS 90, 51pp. https://doi.org/10.25923/70qt-9574"
     _ => error("No citation for WOA$(my_product_year(product_year)) Salinity. Add it if it should be there!")
 end
 
@@ -34,6 +36,7 @@ citation_Oxygen(product_year) = @match my_product_year(product_year) begin
     "09" => "Garcia et al. (2010), World Ocean Atlas 2009, Volume 3: Dissolved Oxygen, Apparent Oxygen Utilization, and Oxygen Saturation. S. Levitus, Ed. NOAA Atlas NESDIS 70, U.S. Government Printing Office, Washington, D.C., 344 pp."
     "13" => "Garcia et al. (2014), World Ocean Atlas 2013, Volume 3: Dissolved Oxygen, Apparent Oxygen Utilization, and Oxygen Saturation. S. Levitus, Ed., A. Mishonov Technical Ed.; NOAA Atlas NESDIS 75, 27 pp."
     "18" => "Garcia et al. (2018), World Ocean Atlas 2018, Volume 3: Dissolved Oxygen, Apparent Oxygen Utilization, and Oxygen Saturation. A. Mishonov Technical Ed.; in preparation."
+    "23" => "Garcia et al. (2023), World Ocean Atlas 2023, Volume 3: Dissolved Oxygen, Apparent Oxygen Utilization, and Oxygen Saturation. A. Mishonov Technical Ed.; NOAA Atlas NESDIS 91, 34pp, https://doi.org/10.25923/rb67-ns53"
     _ => error("No citation for WOA$(my_product_year(product_year)) AOU. Add it if it should be there!")
 end
 
@@ -41,6 +44,7 @@ citation_Nutrients(product_year) = @match my_product_year(product_year) begin
     "09" => "Garcia et al. (2010), World Ocean Atlas 2009, Volume 4: Nutrients (phosphate, nitrate, silicate). S. Levitus, Ed. NOAA Atlas NESDIS 71, U.S. Government Printing Office, Washington, D.C., 398 pp."
     "13" => "Garcia et al. (2014), World Ocean Atlas 2013, Volume 4: Dissolved Inorganic Nutrients (phosphate, nitrate, silicate). S. Levitus, Ed., A. Mishonov Technical Ed.; NOAA Atlas NESDIS 76, 25 pp."
     "18" => "Garcia et al. (2018), World Ocean Atlas 2018, Volume 4: Dissolved Inorganic Nutrients (phosphate, nitrate and nitrate+nitrite, silicate). A. Mishonov Technical Ed.; in preparation."
+    "23" => "Garcia et al. (2023), World Ocean Atlas 2023, Volume 4: Dissolved Inorganic Nutrients (phosphate, nitrate and nitrate+nitrite, silicate). A. Mishonov Technical Ed.; NOAA Atlas NESDIS 92, 34pp, https://doi.org/10.25923/39qw-7j08"
     _ => error("No citation for WOA$(my_product_year(product_year)) Nutrients. Add it if it should be there!")
 end
 
